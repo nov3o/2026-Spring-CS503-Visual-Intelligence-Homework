4355d1f85b1806d3f680ae239204e2bc8a6870a0
Your Name <you@example.com>
2026-04-16 15:37:18 +0200

    minor upd

--- run_vlm_flow.sh
+++ run_vlm_flow.sh
@@ -2,6 +2,9 @@
 # Usage: ./run_vlm_flow.sh {vlm | flow <config.yaml>}
 set -e
 N=$(nvidia-smi -L | wc -l)
+export HF_HOME=/scratch/cscs/ibadanin/hf_cache
+export TRANSFORMERS_CACHE=/scratch/cscs/ibadanin/hf_cache
+
 case "$1" in
   vlm)  cd NanoVLM_Homework   && OMP_NUM_THREADS=1 torchrun --standalone --nproc_per_node=$N train.py ;;
   flow) cd NanoFM_Homeworks   && OMP_NUM_THREADS=1 torchrun --standalone --nproc_per_node=$N run_training.py --config "$2" ;;
